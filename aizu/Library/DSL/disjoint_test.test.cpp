ec7e31e52d18ade1dffc57bc5f3e39e5a2870a77
shinyclovers <36224157+shinyclovers@users.noreply.github.com>
2023-10-06 18:17:27 -0400

    More DSU tests

---
+++ aizu/Library/DSL/disjoint_test.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A"
+#include <iostream>
+#include "datastructures/disjointsetunion.hpp"
+
+int main(){
+  int n, q;
+  std::cin >> n >> q;
+  DisjoinSetUnion dsu(n);
+  for (int i = 0 ; i < q ; i++) {
+    int t, u, v;
+    std::cin >> t >> u >> v;
+    if (t == 0) dsu.merge(u, v);
+    else std::cout << (dsu.find(u) == dsu.find(v)) << std::endl;
+  }
+}
