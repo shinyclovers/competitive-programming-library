b1a13d2793edf4aebeeeffa688c185d4a981de0e
Ryon Chan <36224157+shinyclovers@users.noreply.github.com>
2023-10-06 17:24:32 -0400

    Disjoint set union (#3)
    * Initial files
    
    * [auto-verifier] verify commit 2aac4f54b153e19d606e3cb1b608246b041ba034
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library-checker/data-structures/unionfind.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include <iostream>
+#include "datastructures/disjointsetunion.hpp"
+
+int main(){
+  int n, q;
+  std::cin >> n >> q;
+  DisjoinSetUnion dsu(n);
+  for (int i = 0 ; i < q ; i++) {
+    int t, u, v;
+    std::cin >> t >> u >> v;
+    if (t == 0) dsu.merge(u, v);
+    else std::cout << (dsu.find(u) == dsu.find(v)) << std::endl;
+  }
+}
